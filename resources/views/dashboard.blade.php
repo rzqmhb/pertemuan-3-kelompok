4de10912091cba027489ab24f1b5c5b4d5e967eb
Roziq <roziqmahbubi22@gmail.com>
2023-03-06 22:07:32 +0700

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.template')
+@section('title')
+    Dashboard
+@stop
+
+@section('content_title')
+    Selamat Datang
+    <br><br>
+    <h6>Proyek Kelompok</h6>
+    <h6>Mata Kuliah Web Lanjut</h6>
+    <h6>Pertemuan 3 Praktikum 2</h6>
+@stop
